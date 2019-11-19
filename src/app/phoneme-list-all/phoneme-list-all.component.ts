97ee310db96be254ec1396063fadd4940107133c
Matthew Leonard <mleonard36@gatech.edu>
2019-11-18 23:59:35 -0500

    basic sound and repeat button functionality

--- src/app/phoneme-list-all/phoneme-list-all.component.ts
+++ src/app/phoneme-list-all/phoneme-list-all.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
     templateUrl: 'phoneme-list-all.component.html',
-    styleUrls: [ 'phoneme-list-all.component.css',
-                 '../main.css' ]
+    styleUrls: ['phoneme-list-all.component.css',
+        '../main.css']
 })
 
-export class ListComponent {}
+export class ListComponent implements OnInit, OnDestroy {
+    instruction: HTMLAudioElement;
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+        this.instruction = new Audio();
+        this.instruction.src = '/assets/audio/00_Button_Audio_Complete_a_whole_puzzle_(Phonics_only).mp3';
+        this.instruction.load();
+        this.playAudio();
+    }
+
+    ngOnDestroy() {
+        this.instruction.pause();
+    }
+
+    playAudio() {
+        this.instruction.pause();
+        this.instruction.currentTime = 0;
+        this.instruction.play();
+    }
+}
