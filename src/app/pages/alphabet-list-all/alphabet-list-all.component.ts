802de9982054fa704eb362775b61a1acfa10749e
Jasmin Voigtlander <jasminvoigtlander@Jasmins-Air.attlocal.net>
2020-01-22 18:00:27 -0500

    audio now plays upon opening page

--- src/app/pages/alphabet-list-all/alphabet-list-all.component.ts
+++ src/app/pages/alphabet-list-all/alphabet-list-all.component.ts
@@ -1,10 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
     templateUrl: 'alphabet-list-all.component.html',
     styleUrls: ['alphabet-list-all.component.css',
         '../../main.css']
 })
-export class AlphabetListAllComponent implements OnInit {
+export class AlphabetListAllComponent implements OnInit, OnDestroy {
+    instruction: HTMLAudioElement;
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+        this.instruction = new Audio();
+        this.instruction.src = '/assets/audio/00_Button_Audio_Complete_a_whole_puzzle_(Phonics_only).mp3';
+        this.instruction.load();
+        this.playAudio();
+    }
+
+    ngOnDestroy() {
+        this.instruction.pause();
+    }
+
+    playAudio() {
+        this.instruction.pause();
+        this.instruction.currentTime = 0;
+        this.instruction.play();
+    }
 	letters = ['a','b','c','d','e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 }
