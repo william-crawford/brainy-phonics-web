25a591556b68e0c5faa6ffe9081da95d88087c3a
angsabrina <sabrina.ang@gatech.edu>
2020-01-23 00:12:30 -0500

    got alphabet letters to show up on each card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -2,19 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
-  template: `<div>Card: {{name}}</div>`,
-  styles: [`
-  :host {
-    display: block;
-    padding: 32px;
-    border: 3px solid black;
-    border-radius: 8px;
-  }
-  `]
+  template: `<div class="app-card"> 
+  				<ng-content></ng-content>
+             </div>`,
 })
 
 export class CardComponent implements OnInit {
-	
   constructor() { }
 
   ngOnInit() {
