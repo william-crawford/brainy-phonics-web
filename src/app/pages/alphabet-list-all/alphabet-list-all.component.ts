bf19203f218972366061827acf1b1154dfe95ada
angsabrina <sabrina.ang@gatech.edu>
2020-01-23 23:08:27 -0500

    changes to styling and updated way the letters are pulled up

--- src/app/pages/alphabet-list-all/alphabet-list-all.component.ts
+++ src/app/pages/alphabet-list-all/alphabet-list-all.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import {CardComponent} from '../../components/card/card.component';
+import {RouterModule, Routes} from '@angular/router';
+import {Router} from '@angular/router';
 
 @Component({
     templateUrl: 'alphabet-list-all.component.html',
@@ -8,5 +10,42 @@ import {CardComponent} from '../../components/card/card.component';
 })
 
 export class AlphabetListAllComponent {
-	letters = ['a','b','c','d','e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+	constructor(private router: Router) {
+	}
+
+	//FIX THIS
+    letters: { [key: number]: string } =
+        {
+            1: 'a',
+            2: 'b',
+            3: 'c',
+            4: 'd',
+            5: 'e',
+            6: 'f',
+            7: 'g',
+            8: 'h',
+            9: 'i',
+            10: 'j',
+            11: 'k',
+            12: 'l',
+            13: 'm',
+            14: 'n',
+            15: 'o',
+            16: 'p',
+            17: 'q',
+            18: 'r',
+            19: 's',
+            20: 't',
+            21: 'u',
+            22: 'v',
+            23: 'w',
+            24: 'x',
+            25: 'y',
+            26: 'z'
+        };
+        
+    sortNull() {
+
+    }
 }
