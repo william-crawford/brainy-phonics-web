443a749c38c76289a0f8cfa02e339b01b112ed50
angsabrina <sabrina.ang@gatech.edu>
2020-02-12 22:24:24 -0500

    tried animation for alphabet letters

--- src/app/pages/alphabet-learn/alphabet-learn.component.ts
+++ src/app/pages/alphabet-learn/alphabet-learn.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AlphabetLetter } from '../../types/alphabet-letter';
+import { delay } from 'q';
+import { Router } from '@angular/router';
 import { TransferLetterService } from '../../services/transfer-letter-service.service';
 
 @Component({
@@ -7,13 +9,37 @@ import { TransferLetterService } from '../../services/transfer-letter-service.se
   templateUrl: './alphabet-learn.component.html',
   styleUrls: ['./alphabet-learn.component.css', '../../main.css']
 })
-export class AlphabetLearnComponent {
-	letter= this.transferService.getData();      
+export class AlphabetLearnComponent implements OnInit{
+    letterAnimate: boolean;
+
+	letter = this.transferService.getData();      
 	
-	constructor(private transferService:TransferLetterService) {
-		if(this.letter) {
-			console.log("got letter from transfer service! it's ", this.letter.value);
-  		}
+	constructor(private transferService:TransferLetterService,
+				private router: Router) {
+		if(!this.letter) {
+			this.router.navigateByUrl('/alphabet-list-all');
+		}
+
+		//audio
+        this.letterAnimate = false;
+	}
+
+	ngOnInit() {
+		console.log("init", this.letterAnimate);
+		this.letterAnimate = false;
+		delay(1000);
+		this.letterAnimate = true;
+		delay(1000);
+		this.letterAnimate = false;
+	}
+
+	playAudio() {
+		console.log("clicked", this.letterAnimate);
+		this.letterAnimate = false;
+		delay(1000);
+		this.letterAnimate = true;
+		delay(1000);
+		this.letterAnimate = false;
 	}
 
 }
