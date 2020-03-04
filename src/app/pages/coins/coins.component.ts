90492bcb29e800d2f66e49fb8ab0a635422a2c37
angsabrina <sabrina.ang@gatech.edu>
2020-03-03 23:16:33 -0500

    coin images added to assets

--- src/app/pages/coins/coins.component.ts
+++ src/app/pages/coins/coins.component.ts
@@ -16,6 +16,12 @@ export class CoinsComponent implements OnInit {
 
   ngOnInit() {
     // display image depending on how many coins user has accumulated
+
+    // 1 gold coin = 2 silver coins
+    // 1 stack coins = 5 gold 
+    // 1 bag of coins = 5 stacks 
+    // 1 armored car = 5 bags 
+    // 1 bank = 5 armored cars
   }
 
 }
