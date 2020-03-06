58b4b3db7eaba05d2ca13488e5e98baca6aed2da
Yumin Jeong <yjeong73@gatech.edu>
2020-03-06 08:26:39 -0500

    Added quiz component for Alphabets

---
+++ src/app/pages/alphabet-quiz/alphabet-quiz.component.ts
@@ -0,0 +1,96 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {Router} from '@angular/router';
+import {ElementRef} from '@angular/core';
+import {delay} from 'q';
+import {TransferLetterService} from '../../services/transfer-letter-service.service';
+import * as data from '../../../assets/json/quiz-examples.json';
+import { Location } from '@angular/common';
+import {AlphabetLetter} from '../../types/alphabet-letter';
+
+
+@Component({
+    templateUrl: 'alphabet-quiz.component.html',
+    styleUrls: ['alphabet-quiz.component.css',
+                '../../main.css']
+})
+
+export class AlphabetQuizComponent implements OnInit, OnDestroy {
+    letterAnimate: boolean;
+    letterPlayAudio: boolean;
+    letterAudio: HTMLAudioElement;
+
+    letter: AlphabetLetter;
+
+    // temporary
+    ex1 = new AlphabetLetter('Bb', '/assets/audio/phonemes/sound-A.mp3', 0);
+
+    ex2 = new AlphabetLetter('Cc', '/assets/audio/phonemes/sound-A.mp3', 0);
+
+    ex3 = new AlphabetLetter('Dd', '/assets/audio/phonemes/sound-A.mp3', 0);
+
+    empty = new AlphabetLetter(' ', '/assets/audio/phonemes/sound-A.mp3', 0)
+
+    constructor(private transferService: TransferLetterService, private router: Router, private location: Location) {
+        this.letter = this.transferService.getData() as AlphabetLetter;
+        if (!this.letter) {
+            this.router.navigateByUrl('/alphabet-list-all');
+        }
+
+        //audio
+        this.letterAnimate = false;
+        this.letterPlayAudio = true;
+    };
+
+    ngOnInit() {
+        this.letterAudio = new Audio();
+        this.letterAudio.src = '/assets/audio/phonemes/sound-A.mp3';
+        this.letterAudio.load();
+
+        this.letterAudio.onended = () => {
+            this.letterAnimate = false;
+        };
+
+        this.playAudio();
+    }
+
+    ngOnDestroy() {
+        this.letterAudio.pause();
+    }
+
+    correctAnswer() {
+        this.letterAnimate = true;
+        this.letterAudio.onended = () => {
+            this.letterAnimate = false;
+            this.letterAudio.onended = () => {
+                this.letterAnimate = false;
+            };
+        };
+
+        this.ex1 = this.empty;
+        this.ex2 = this.empty;
+        this.ex3 = this.empty;
+
+        this.letterAudio.play();
+        delay(300).then(() => {
+            this.loadNew();
+        });
+    }
+
+    loadNew() {
+        // temporary
+        this.ex1 = new AlphabetLetter('Ee', '/assets/audio/phonemes/sound-A.mp3', 0);
+        this.ex2 = new AlphabetLetter('Ff', '/assets/audio/phonemes/sound-A.mp3', 0);
+        this.ex3 = new AlphabetLetter('Gg', '/assets/audio/phonemes/sound-A.mp3', 0);
+
+        this.playAudio();
+    }
+
+    playAudio() {
+        this.letterAudio.play();
+    }
+
+    goBack() {
+        this.location.back();
+    }
+}
