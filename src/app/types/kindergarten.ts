f7c5381768466816bc797e5633365a8cd0874294
Yumin Jeong <yjeong73@gatech.edu>
2020-04-06 07:30:28 -0400

    Added ts for phoneme categories and grade levels

---
+++ src/app/types/kindergarten.ts
@@ -0,0 +1,24 @@
+import {SightWord} from './sight-word';
+
+export class Kindergarten {
+    id: string;         // unique identifier
+    display: string;    // display string
+    audio: string;      // url to audio
+    word1: SightWord;
+    word2: SightWord;
+    word3: SightWord;
+    stars: number;
+    rhyme: string;
+
+
+    constructor(id: string, display: string, audio: string, word1: SightWord, word2: SightWord, word3: SightWord, stars: number, rhyme: string) {
+        this.id = id;
+        this.display = display;
+        this.audio = audio;
+        this.word1 = word1;
+        this.word2 = word2;
+        this.word3 = word3;
+        this.stars = stars;
+        this.rhyme = rhyme;
+    }
+}
