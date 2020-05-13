b6ef81c15469f726dde45f9104640e4bdf07e0f6
TenkaiTheMixeon <65086259+TenkaiTheMixeon@users.noreply.github.com>
2020-05-13 14:35:03 -0400

    Added K Category Screen

---
+++ src/app/pages/k-categories/k-categories.component.ts
@@ -0,0 +1,27 @@
+import {Component} from '@angular/core';
+import {Location} from '@angular/common';
+
+@Component({
+    templateurl: 'k-categories.component.html',
+    styleUrls: [ 'k-categories.component.css'],
+})
+
+export class KCategoriesComponent {
+    category: HTMLAudioElement;
+    
+    constructor(private location: Location) {
+    }
+    
+    playAudio(event){
+        // this.category = new Audio();
+        // this.category.src = '/assets/audio/buttons/' + event.target.id + '.mp3';
+        // if (this.category.src !== undefined) {
+        //     this.category.load();
+        //     this.category.play();
+        // }
+    }
+
+    goBack() {
+        this.location.back();
+    }
+}
