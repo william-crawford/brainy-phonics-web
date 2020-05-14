0f2a20aa454f107f5d4850a7d261c045befe1715
TenkaiTheMixeon <65086259+TenkaiTheMixeon@users.noreply.github.com>
2020-05-14 10:23:23 -0400

    Creating First Grade categories

---
+++ src/app/pages/first-categories/first-categories/first-categories.component.ts
@@ -0,0 +1,27 @@
+import {Component} from '@angular/core';
+import {Location} from '@angular/common';
+
+@Component({
+    templateUrl: 'first-categories.component.html',
+    styleUrls: [ 'first-categories.component.css'],
+})
+
+export class FirstCategoriesComponent {
+    category: HTMLAudioElement;
+    
+    constructor(private location: Location) {
+    }
+    
+    playAudio(event){
+        // this.category = new Audio();
+        // this.category.src = '/assets/audio/buttons/' + event.target.id + '.mp3';
+        // if (this.category.src !== undefined) {
+        //     this.category.load();
+        //     this.category.play();
+        // }
+    }
+
+    goBack() {
+        this.location.back();
+    }
+}
