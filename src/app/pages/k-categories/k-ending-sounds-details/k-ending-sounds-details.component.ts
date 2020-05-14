ac7e4a979d07746fc3ad33325e3077f3b6ab373c
TenkaiTheMixeon <65086259+TenkaiTheMixeon@users.noreply.github.com>
2020-05-14 12:44:27 -0400

    Create k-ending-sounds-details.component.ts

---
+++ src/app/pages/k-categories/k-ending-sounds-details/k-ending-sounds-details.component.ts
@@ -0,0 +1,27 @@
+import {Component} from '@angular/core';
+import {Location} from '@angular/common';
+
+@Component({
+    templateUrl: 'k-ending-sounds-details.component.html',
+    styleUrls: [ 'k-ending-sounds-details.component.css']
+})
+
+export class KEndingSoundsDetailsComponent {
+    category: HTMLAudioElement;
+
+    constructor(private location: Location) {
+    }
+
+    playAudio(event) {
+        // this.category = new Audio();
+        // this.category.src = '/assets/audio/buttons/' + event.target.id + '.mp3';
+        // if (this.category.src !== undefined) {
+        //     this.category.load();
+        //     this.category.play();
+        // }
+    }
+
+    goBack() {
+        this.location.back();
+    }
+}
