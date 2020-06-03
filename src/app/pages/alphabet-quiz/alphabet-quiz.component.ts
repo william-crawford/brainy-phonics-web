37bd0bbccf5a7b302a8c9e8fa2292fb377c13fd0
TenkaiTheMixeon <65086259+TenkaiTheMixeon@users.noreply.github.com>
2020-06-03 11:08:32 -0400

    Update alphabet-quiz.component.ts

--- src/app/pages/alphabet-quiz/alphabet-quiz.component.ts
+++ src/app/pages/alphabet-quiz/alphabet-quiz.component.ts
@@ -127,12 +127,12 @@ export class AlphabetQuizComponent implements OnInit, OnDestroy, AfterViewInit {
             this.letterAnimate4 = true;
         }
 
-        this.letterAudio.onended = () => {
+        this.correctSound.onended = () => {
             this.letterAnimate1 = false;
             this.letterAnimate2 = false;
             this.letterAnimate3 = false;
             this.letterAnimate4 = false;
-            this.letterAudio.onended = () => {
+            this.correctSound.onended = () => {
                 this.letterAnimate1 = false;
                 this.letterAnimate2 = false;
                 this.letterAnimate3 = false;
@@ -151,10 +151,11 @@ export class AlphabetQuizComponent implements OnInit, OnDestroy, AfterViewInit {
 
         delay(200).then(() => {
             this.correctSound.play();
-            delay(800).then(() => {
-                this.playAudio();
-            });
             this.loadNew();
+        });
+
+        delay(2000).then(() => {
+            this.playAudio();
         });
 
         if(this.isFirstAttempt) {
