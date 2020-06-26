e2308206a401a587546548060161bde94a3c8133
Sam Billingham <sambillie.sb@gmail.com>
2020-06-26 17:28:17 +0200

    add: (main) - hide addressbar ios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,3 +21,13 @@ export class AppComponent {
     }
   }
 }
+
+
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+  /* iOS hides Safari address bar */
+  window.addEventListener("load",function() {
+      setTimeout(function() {
+          window.scrollTo(0, 1);
+      }, 1000);
+  });
+}
